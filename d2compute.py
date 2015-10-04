eb43eac3c18246b4a5958b80cd95cf5268e2a217
Kujin Tang <kujin@hpc-login3.hpcc.usc.edu>
2015-10-04 14:43:09 -0700

    finish all d2,d2star,d2shepp calculation

--- d2compute.py
+++ d2compute.py
@@ -44,12 +44,24 @@ class Species:
         self.E = sum(self.kmer) * self.kprob
         self.tilde = self.kmer - self.E
 
+#Compare the cosinse similarity between x and y.
 def cosine(x, y):
     return sum(x * y) / (np.sqrt(sum(x**2)) * np.sqrt(sum(y**2)))
 
+#Compare d2 between X and Y.
 def d2compare(X, Y):
     return (1-cosine(X.kmer, Y.kmer))
     
+#Compare d2star between X and Y.
+def d2starcompare(X, Y):
+    return 0.5 * (1 - cosine(X.tilde/np.sqrt(X.E), Y.tilde/np.sqrt(Y.E)))
+
+#Compare d2shepp between X and Y.
+def d2sheppcompare(X, Y):
+    denom = (X.tilde**2 + Y.tilde**2) ** 0.25
+    return 0.5 * (1 - cosine(X.tilde/denom, Y.tilde/denom))
+
+#Compare the pairwise distance by given method.
 def pairwise(species, sequences, kvalue, inputtype, method):
     spelist = []
     spenumber = len(species)
@@ -63,8 +75,9 @@ def pairwise(species, sequences, kvalue, inputtype, method):
             distmatrix[j][i] = distance
     return distmatrix
     
-def formatprint(species, distmatrix, outfile):
-    f = open(outfile, 'wt')
+#Print the distance matrix in given format.
+def formatprint(species, distmatrix, outfile, method, kvalue):
+    f = open(outfile+method+'_k'+str(kvalue), 'wt')
     spenumber = len(distmatrix)
     f.write(str(spenumber)+'\n')
     for i in range(spenumber):
@@ -93,20 +106,31 @@ def main():
     parser.add_option('-t', action='store', dest='inputtype',\
 help='L for long sequences, A for fasta files')
 
+    #Method
+    parser.add_option('-m', action='store', dest='d2method')
+
     options, reminder = parser.parse_args()
     
     inputfile = options.inputfile
     outputprefix = options.outputprefix
     inputtype = options.inputtype
+    d2method = options.d2method
 
     kvalue = errmsg.check_k(options.kvalue)
     errmsg.check_inputfile(inputfile)
     errmsg.check_inputtype(inputtype)
-
+    errmsg.check_outputdir(outputprefix)
+    d2method = errmsg.check_d2method(d2method)
     
     species, sequences = analyzeInput(inputfile)
-    distmatrix = pairwise(species, sequences, kvalue, inputtype, d2compare)
-    formatprint(species, distmatrix, outputprefix)
+    for method in d2method:
+        if method == 'd2':
+            distmatrix = pairwise(species, sequences, kvalue, inputtype, d2compare)
+        elif method == 'd2star':
+            distmatrix = pairwise(species, sequences, kvalue, inputtype, d2starcompare)
+        else:
+            distmatrix = pairwise(species, sequences, kvalue, inputtype, d2sheppcompare)
+        formatprint(species, distmatrix, outputprefix, method, kvalue)
     '''
     print distmatrix
     test1 = Species(species[0], sequences[0], kvalue, inputtype)
--- errmsg.py
+++ errmsg.py
@@ -3,6 +3,7 @@ import sys
 import os
 
 INPUTTYPE = ['A', 'L']
+METHOD = ['d2', 'd2shepp', 'd2star']
 
 def check_k(k):
     try:
@@ -22,3 +23,14 @@ def check_inputtype(inputtype):
     if not inputtype in INPUTTYPE:
         sys.exit("Please enter the correct input data type: only A or L.")
 
+def check_d2method(d2method):
+    method = map(lambda x:x.strip(), d2method.split(','))
+    for i in method:
+        if not i in METHOD:
+            sys.exit("No method named %s!"%i)
+    return method
+
+def check_outputdir(outputprefix):
+    dirname = os.path.dirname(outputprefix)
+    if dirname and not os.path.isdir(dirname):
+        sys.exit(dirname + " is not a directory!")
